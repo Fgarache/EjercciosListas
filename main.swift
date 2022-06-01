f9bde242f75bc8d2e3173cced5090a124194d97b
Francisco Garache <franciscogarach3@gmail.com>
2022-06-01 17:09:30 +0000

    DELETE

--- main.swift
+++ main.swift
@@ -1,14 +1,15 @@
-var dato: String
+var dato = "null"
 var lista = [String]()
 var obcion: String
 var ejecucion =  true
+var num1 = 1
 
 func menu (){
     print("1. add itens the array")
     print("2. read itens the array")
-    print("4. remove itens the array")
-    print("5. eliminar primero")
-    print("6. eliminar ultimo")
+    print("3. remove itens the array")
+    print("4. eliminar primero")
+    print("5. eliminar ultimo")
     print("0 para salir")
     print("====================")
 }
@@ -21,6 +22,9 @@ obcion = readLine()!
 func pedirDatos(){
   obcion = readLine()!  
 }
+
+
+
 
 while ejecucion == true {
 switch (obcion){
@@ -40,12 +44,22 @@ switch (obcion){
             pedirDatos()
             break
     case "3":
+            print("\nIngrede el indice a eliminar")
+            pedirDatos()
+            num1 = Int(dato) ?? 0
+            lista.remove(at: num1)
+            print("se elimini", lista[num1], "\n")
+            menu()
+            pedirDatos()
+    
             break
     case "0":print("Salir")
             ejecucion = false
             break
     
     default: print("Elija una obcion")
+            menu()
+            pedirDatos()
     
 }
     
